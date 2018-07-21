5570196b986f4054a4d531da7c8208162f65e47b
caroleguardiola <carole.guardiola@gmail.com>
2018-07-21 16:40:54 +0200

    [Travis] fixed autoload.

--- web/app_dev.php
+++ web/app_dev.php
@@ -18,7 +18,8 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require __DIR__.'/../vendor/autoload.php';
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
